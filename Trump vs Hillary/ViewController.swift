6d13cc701741f297fdfa32d5966a0c6a2de3bfca
Kiddos <koubarlow@gmail.com>
2016-10-12 15:01:23 -0600

    Initial Commit

---
+++ Trump vs Hillary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trump vs Hillary
+//
+//  Created by Kiddos on 10/12/16.
+//  Copyright © 2016 Team Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Trump vs HillaryTests/Trump_vs_HillaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Trump_vs_HillaryTests.swift
+//  Trump vs HillaryTests
+//
+//  Created by Kiddos on 10/12/16.
+//  Copyright © 2016 Team Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Trump_vs_Hillary
+
+class Trump_vs_HillaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
